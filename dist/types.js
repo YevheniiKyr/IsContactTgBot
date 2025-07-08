9b5f265ff6392d00f6070d667e4e28d6e1c66c8e
YevheniiKyr <jekafekz@gmail.com>
2025-07-08 19:57:19 +0300

    removed dist from tracking

--- dist/types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Role = void 0;
-var Role;
-(function (Role) {
-    Role["Thinker"] = "thinker";
-    Role["Contactor"] = "contactor";
-    Role["None"] = "none";
-})(Role || (exports.Role = Role = {}));
